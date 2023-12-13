75a1651eeeb099c070572aa20cc2fb9c090570d0
Davide Garberi <dade.garberi@gmail.com>
2023-12-13 12:28:08 +0100

    WhisperBot: Add initial start command

--- WhisperBot.py
+++ WhisperBot.py
@@ -20,8 +20,19 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        "Welcome to the Whisper Transcribe Bot!\n"
+        "With this bot you can transcribe audio messages, using the OpenAI Whisper neural net.\n"
+        "You can run this bot in private chat, just by sending a voice message, or add it in groups.\n"
+        "Type /help for a list of commands\n"
+        "Bot made by DD3Boh."
+    );
+
 def main() -> None:
     application = Application.builder().token(sys.argv[1]).build()
+
+    application.add_handler(CommandHandler("start", start))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
